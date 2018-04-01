564131ce31130cda85b047e86bb39aee3d7c537d
li-jin-1 <pandoralel1@gmail.com>
2018-04-01 02:28:10 -0500

    nodejs

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(4200);
+
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+    });
+
+    client.on('messages', function(data) {
+      client.emit('broad', 'You: ' + data);
+           client.emit('broad', 'Li: FUCK YOU! Go get me water!!');
+           client.broadcast.emit('broad',data);
+    });
+
+});
